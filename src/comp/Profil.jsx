b8175f8bcf708d3fe8741857ae0ac1a8ad509eeb
gabriellevy <mdeharbe@hotmail.com>
2023-10-21 19:14:10 +0200

    meilleur affichage avec parchemin

--- src/comp/Profil.jsx
+++ src/comp/Profil.jsx
@@ -71,115 +71,121 @@ function Profil() {
     }
   }
     return (
-      <div className="profil">
-      {persoSelectionne ? (
-        <div>
-    <span ref={componentRef}>
-          <table className="tableSansBord">
-            <tbody>
-              <tr>
-                <td>
-                  <Portrait perso={perso} />
-                </td>
-                <td>
-                  <div className="descriptionPerso">
-                    {perso.coterie} ({perso.region})
-                    <br />
-                    {perso.age} ans
-                    <br />
-                    {perso.poids} kg
-                    <br />
-                    {afficheObjets(perso)}
-                  </div>
-                </td>
-              </tr>
-            </tbody>
-            </table>
-            <h3>{perso.nom} - 
-                    {perso.evolution !== undefined && perso.evolution !== ''
-                      ? perso.evolution
-                      : perso.carriere}
-                     ({perso.statut_echelon} {perso.statut_standing})</h3>
-            <table className="descriptionPerso">
+      <div>
+        {persoSelectionne ? (
+            <div>
+        <div id="parchment" ref={componentRef}>
+            <table className="tableSansBord">
                 <tbody>
-                    <tr>
-                    <td>CC </td>
-                    <td>CT </td>
-                    <td>FOR </td>
-                    <td>END </td>
-                    <td>INIT </td>
-                    <td>AG </td>
-                    <td>DEX </td>
-                    <td>INT </td>
-                    <td>F.M </td>
-                              <td>SOC </td>
-                              <td>Bl</td>
-                    </tr>
-                    <tr>
-                    <td>{perso.cc}</td>
-                    <td> {perso.ct}</td>
-                    <td> {perso.force}</td>
-                    <td> {perso.endurance}</td>
-                    <td> {perso.initiative}</td>
-                    <td> {perso.agilete}</td>
-                    <td> {perso.dexterite}</td>
-                    <td> {perso.intelligence}</td>
-                    <td> {perso.force_mentale}</td>
-                    <td> {perso.sociabilite}</td>
-                    <td> {perso.pointsDeBlessure}</td>
-                    </tr>
+                <tr>
+                    <td>
+                    <Portrait perso={perso} />
+                    </td>
+                    <td>
+                    <div className="descriptionPerso">
+                        {perso.coterie} ({perso.region})
+                        <br />
+                        {perso.age} ans
+                        <br />
+                        {perso.poids} kg
+                        <br />
+                        {afficheObjets(perso)}
+                    </div>
+                    </td>
+                </tr>
                 </tbody>
             </table>
-                  <div className="descriptionPerso">
+            <div>
+                <div class="nomPerso">{perso.nom} -&nbsp;
+                    {perso.evolution !== undefined && perso.evolution !== ''
+                        ? perso.evolution
+                        : perso.carriere}
+                    &nbsp;({perso.statut_echelon} {perso.statut_standing})
+                </div>
+                <table className="descriptionPerso">
+                    <tbody>
+                        <tr>
+                            <td className="caracs">CC </td>
+                            <td className="caracs">CT </td>
+                            <td className="caracs">FOR </td>
+                            <td className="caracs">END </td>
+                            <td className="caracs">INIT </td>
+                            <td className="caracs">AG </td>
+                            <td className="caracs">DEX </td>
+                            <td className="caracs">INT </td>
+                            <td className="caracs">F.M </td>
+                            <td className="caracs">SOC </td>
+                            <td className="caracs">Bl</td>
+                        </tr>
+                        <tr>
+                            <td className="caracs">{perso.cc}</td>
+                            <td className="caracs">{perso.ct}</td>
+                            <td className="caracs">{perso.force}</td>
+                            <td className="caracs">{perso.endurance}</td>
+                            <td className="caracs">{perso.initiative}</td>
+                            <td className="caracs">{perso.agilete}</td>
+                            <td className="caracs">{perso.dexterite}</td>
+                            <td className="caracs">{perso.intelligence}</td>
+                            <td className="caracs">{perso.force_mentale}</td>
+                            <td className="caracs">{perso.sociabilite}</td>
+                            <td className="caracs">{perso.pointsDeBlessure}</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <div className="descriptionPerso">
                     <b>Compétences : </b>
-                      {lstComps.map(({ titre, description }) => {
-                        const idComp = getCompObjPropertyName(titre)
-                        // la valeur finale dépend de la compétence mais aussi de la valeur de la carac liée :
-                        const valeurComp = perso[idComp]
-                        if (valeurComp > 0) {
-                          const compObj = getCompetence(titre)
-                          const idCarac = getCaracObjPropertyName(compObj.carac)
-                          const valeurCarac = perso[idCarac]
-                          const valeur = valeurComp + valeurCarac
-                          return (
-                            <span key={titre} title={description}>
-                                 {titre} {valeur}, 
-                            </span>
-                          )
-                        } else {
-                          return ''
-                        }
-                      })}
-                  </div>
-                  <div className="descriptionPerso">
-                    <h4>Talents</h4>
-                    <ul>
-                      {lstTalents.map(({ titre, description }) => {
-                        const idTalent = getTalentObjPropertyName(titre)
-                        // la valeur finale dépend du talent :
-                        const valeurTalent = perso[idTalent]
-                        if (valeurTalent > 0) {
-                          return (
-                            <span key={titre} title={description}>
-                              <li>
-                                {titre}{(valeurTalent > 1) ? ({ valeurTalent }) :''}
-                              </li>
-                            </span>
-                          )
-                        } else {
-                          return ''
-                        }
-                      })}
-                    </ul>
-                  </div>
-        </span>
-            <Button onClick={() => VersPdf()}>
-            Pdf
-            </Button>
-        </div>
-      ) : (
-        <h1>Création de personnage Warhammer</h1>
-      )}
+                    {lstComps.map(({ titre, description }) => {
+                    const idComp = getCompObjPropertyName(titre)
+                    // la valeur finale dépend de la compétence mais aussi de la valeur de la carac liée :
+                    const valeurComp = perso[idComp]
+                    if (valeurComp > 0) {
+                        const compObj = getCompetence(titre)
+                        const idCarac = getCaracObjPropertyName(compObj.carac)
+                        const valeurCarac = perso[idCarac]
+                        const valeur = valeurComp + valeurCarac
+                        return (
+                        <span key={titre} title={description}>
+                                {titre} {valeur},&nbsp;
+                        </span>
+                        )
+                    } else {
+                        return ''
+                    }
+                    })}
+                </div>
+                <div id="talents" className="descriptionPerso">
+                <h4>Talents : </h4>
+                <ul>
+                    {lstTalents.map(({ titre, description }) => {
+                    const idTalent = getTalentObjPropertyName(titre)
+                    // la valeur finale dépend du talent :
+                    const valeurTalent = perso[idTalent]
+                    if (valeurTalent > 0) {
+                        return (
+                        <span key={titre} title={description}>
+                            <li>
+                            {titre}{(valeurTalent > 1) ? ({ valeurTalent }) :''}
+                            </li>
+                        </span>
+                        )
+                    } else {
+                        return ''
+                    }
+                    })}
+                </ul>
+                </div>
+                <div id="possessions" className="descriptionPerso">
+                    <h4>Possessions : </h4>
+                </div>
+            </div>
+            </div>
+                <Button onClick={() => VersPdf()}>
+                Pdf
+                </Button>
+            </div>
+        ) : (
+            <h1>Création de personnage Warhammer</h1>
+        )}
     </div>
   )
 }
